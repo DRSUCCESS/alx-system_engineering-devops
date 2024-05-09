d7756204fdb788617bba66a0b12151f9a6d7ddc0
DRSUCCESS <57950989+DRSUCCESS@users.noreply.github.com>
2024-05-09 04:51:55 +0100

    new files

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Creates a file
-file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+# Create a PP File
+file { '/tmp/holberton':
+  ensure  => file, # Makes sure File Exists
+  path    => '/tmp/holberton', # PATH
+  mode    => '0744',           # Permissions
+  owner   => 'www-data',       # Owner
+  group   => 'www-data',       # Group
+  content => 'I love Puppet',  # Content
 }
